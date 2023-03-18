9da8d37910f4371f95eb9a336cd5acf3d3bed563
Kevin Rodriguez <kevinportillo808@gmail.com>
2023-03-18 10:59:13 -0600

    Commit1

---
+++ Proyect02/src/proyect02/Proyect02.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyect02;
+
+import com.sun.speech.freetts.VoiceManager;
+import com.sun.speech.freetts.Voice;
+
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+public class Proyect02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        
+        JFrame02 v1 = new JFrame02();
+        
+        v1.setVisible(true);
+         
+       /*
+        
+        Voice voice;
+
+        VoiceManager vm = VoiceManager.getInstance();
+        
+        voice = vm.getVoice("kevin16");
+        
+        voice.allocate();
+        
+        voice.speak("Hello world");
+        
+        voice.allocate();
+      */
+        
+    }
+    
+}
